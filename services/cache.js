4cfbea43fb4cdb818f0472d3259c2fa3edeb0d36
Nikhil Shrivastav <nikhil.shrivastav@live.com>
2019-01-18 18:40:50 +0530

    added check to see if any cached data is available

--- services/cache.js
+++ services/cache.js
@@ -12,6 +12,18 @@ const exec = mongoose.Query.prototype.exec;
 mongoose.Query.prototype.exec = async function () {
 
 	console.log('The query is: ', this.getQuery(), this.mongooseCollection.name);
+	const key = JSON.stringify(Object.assign({},this.getQuery(), {
+		collection: this.mongooseCollection.name
+	}));
+
+	const cachedResult = client.get(key);
+
+	if (cachedResult) {
+		console.log('The cachedResult is: ', cachedResult)
+	} else {
+		console.log('No cache data');
+	}
 	const result = await exec.apply(this, arguments);
+	client.set(key, JSON.stringify(result));
 	return result;
 }
