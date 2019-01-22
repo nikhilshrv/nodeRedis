bbbfe43e0be2cb75b777d0a9e087144428c7c7cc
Nikhil Shrivastav <nikhil.shrivastav@live.com>
2019-01-22 23:25:59 +0530

    added .cache() to check for cache on certain routes only

--- services/cache.js
+++ services/cache.js
@@ -9,24 +9,34 @@ client.get = util.promisify(client.get);
 const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+	this.useCache = true;
+	
+	return this;
+}
+
 mongoose.Query.prototype.exec = async function () {
 
-	console.log('The query is: ', this.getQuery(), this.mongooseCollection.name);
-	const key = JSON.stringify(Object.assign({},this.getQuery(), {
-		collection: this.mongooseCollection.name
-	}));
+	if (this.useCache) {
+		console.log('The query is: ', this.getQuery(), this.mongooseCollection.name);
+		const key = JSON.stringify(Object.assign({},this.getQuery(), {
+			collection: this.mongooseCollection.name
+		}));
+		const cachedResult = await client.get(key);
 
-	const cachedResult = client.get(key);
+		if (cachedResult) {
+			console.log('The cachedResult is: ', cachedResult);
+			const doc = JSON.parse(cachedResult);
+			return Array.isArray(doc) ? doc.map(d => this.model(d)) : this.model(doc);
+		} else {
+			console.log('No cache data');
+			const result = await exec.apply(this, arguments);
+			client.set(key, JSON.stringify(result));
+			return result;
+		}
 
-	if (cachedResult) {
-		console.log('The cachedResult is: ', cachedResult);
-		const doc = JSON.parse(cachedResult);
-		return Array.isArray(doc) ? doc.map(d => this.model(d)) : this.model(doc);
 	} else {
-		console.log('No cache data');
-		const result = await exec.apply(this, arguments);
-		client.set(key, JSON.stringify(result));
-		return result;
-	}
-	
+		return exec.apply(this, arguments);
+	}	
+
 }
